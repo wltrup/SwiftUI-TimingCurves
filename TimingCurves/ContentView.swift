283fac9d0d2ca40ba245e54c06610e95aa0cc055
Wagner Truppel <trupwl@gmail.com>
2020-07-17 04:38:44 +0100

    Initial Commit

---
+++ TimingCurves/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimingCurves
+//
+//  Created by Wagner Truppel on 17/07/2020.
+//  Copyright © 2020 wtruppel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
